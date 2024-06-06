cd4591b15aeace09220b231ddbe3e34e490ae23f
zhaozhao <1939065930@qq.com>
2024-06-06 20:41:23 +0800

    function change ,click callback added

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use wasm_bindgen::prelude::*;
 
 slint::include_modules!();
 
-use std::rc::Rc;
+use std::{ops::ControlFlow, rc::Rc};
 
 use slint::{Model, StandardListViewItem, VecModel};
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-pub fn main() {
+pub fn main() -> Result<(), slint::PlatformError> {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(all(debug_assertions, target_arch = "wasm32"))]
@@ -21,5 +21,24 @@ pub fn main() {
     //slint::init_translations!(concat!(env!("CARGO_MANIFEST_DIR"), "/lang/"));
     let app = App::new().unwrap();
 
-    app.run().unwrap();
+    app.global::<ControlsPageAdapter>().on_connection_apply({
+        let app_weak = app.as_weak();
+        |connection: ssh_info| {
+            println!("pressed!!");
+            println!(
+                "{} {} {} {} {}",
+                connection.ip,
+                connection.username,
+                connection.password,
+                connection.board_type,
+                connection.slot
+            );
+        }
+    });
+
+    app.run()
+}
+
+fn session_initialize() -> Result<()> {
+    Ok(())
 }
